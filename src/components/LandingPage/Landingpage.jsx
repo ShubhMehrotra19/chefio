3a066e353fe9f0936053c314913563848431f5e9
Stoic Coder <shubhmehrotra2022@vitbhopal.ac.in>
2024-02-20 10:36:41 +0530

    added navlivk to signup button for UI trial

--- src/components/LandingPage/Landingpage.jsx
+++ src/components/LandingPage/Landingpage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Landingpage.css';
+import { NavLink } from 'react-router-dom';
 
 function Landingpage() {
   const sectionStyle = {
@@ -22,7 +23,7 @@ function Landingpage() {
         <h1 className="font-Poppins text-9xl font-bold leading-normal mb-[-30px]">Chefio.</h1>
         <h3 className="font-Poppins text-[24px] font-semibold leading-normal m-0 w-[500px]">The community that celebrates cooking</h3>
         <div className="flex justify-start gap-[40px] mt-[20px]">
-          <button className="bn5 rounded-lg bg-[rgba(0,0,0,0.17)] px-10 py-4 text-white cursor-pointer font-semibold hover:bg-slate-600 hover:scale-105 transition duration-300 ease-in-out relative">Sign up</button>
+          <NavLink to='/home'><button className="bn5 rounded-lg bg-[rgba(0,0,0,0.17)] px-10 py-4 text-white cursor-pointer font-semibold hover:bg-slate-600 hover:scale-105 transition duration-300 ease-in-out relative">Sign up</button></NavLink>
           <button className="bn4 rounded-lg px-10 py-4 bg-white text-black cursor-pointer font-semibold hover:bg-zinc-300 hover:scale-105 hover:border-zinc-300 transition duration-300 ease-in-out relative">Sign in</button>
         </div>
       </div>
