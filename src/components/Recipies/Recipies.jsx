3eca54ed6e8baa80eb1e9a1287648bd50ce8174a
Stoic Coder <shubhmehrotra2022@vitbhopal.ac.in>
2024-02-22 19:51:57 +0530

    reviw card component connected with ImageCarousel index

--- src/components/Recipies/Recipies.jsx
+++ src/components/Recipies/Recipies.jsx
@@ -6,12 +6,45 @@ import ImageCarousel from './ImageCarousel';
 function Recipies() {
   const cardData = [
     {
-      profile: 'https://images.pexels.com/photos/1065084/pexels-photo-1065084.jpeg?auto=compress&cs=tinysrgb&w=600',
-      Pname: 'Alex Alewood',
-      rating: '4.0',
-      review: 'Since I do overnight shifts for my company, sometimes I have to eat noodles to get through. This simple dish made me fall in love with Chefio.',
-      position: { top: '30%', left: '28%' },
+        key: 0,
+        profile: 'https://images.pexels.com/photos/4057693/pexels-photo-4057693.jpeg?auto=compress&cs=tinysrgb&w=600',
+        Pname: 'Alexandra Hue',
+        rating: '4.6',
+        review: 'As a Granny, I love it when my grandkids eat my dishes. This dish made them lick their fingers :) haha.',
+        position: { top: '30%', left: '28%' },
     },
+    {
+        key: 1,
+        profile: 'https://images.pexels.com/photos/6937744/pexels-photo-6937744.jpeg?auto=compress&cs=tinysrgb&w=600',
+        Pname: 'Rehman Afiz',
+        rating: '4.2',
+        review: 'The grilled salmon and asparagus were cooked to perfection, making for a healthy and delicious meal.',
+        position: { top: '30%', left: '28%' },
+      },
+      {
+        key: 2,
+        profile: 'https://images.pexels.com/photos/2024901/pexels-photo-2024901.jpeg?auto=compress&cs=tinysrgb&w=600',
+        Pname: 'Alex Alewood',
+        rating: '4.0',
+        review: 'Since I do overnight shifts for my company, sometimes I have to eat noodles to get through. This simple dish made me fall in love with Chefio.',
+        position: { top: '30%', left: '28%' },
+      },
+      {
+        key: 3,
+        profile: 'https://images.pexels.com/photos/15335320/pexels-photo-15335320/free-photo-of-muscular-man-in-a-tank-top-wiping-his-head-from-sweat-with-a-towel.jpeg?auto=compress&cs=tinysrgb&w=600',
+        Pname: 'David Coppler',
+        rating: '4.2',
+        review: 'Everyone loves a healthy salad and so do I. It is that one dish that fills me up after an extensive gym session.',
+        position: { top: '30%', left: '28%' },
+      },
+      {
+        key: 4,
+        profile: 'https://images.pexels.com/photos/7020290/pexels-photo-7020290.jpeg?auto=compress&cs=tinysrgb&w=600',
+        Pname: 'Armano Porter',
+        rating: '4.2',
+        review: 'I am a retired chef and the one thing I love the most about this dish is about its versatility and great taste.',
+        position: { top: '30%', left: '28%' },
+      },
   ];
 
   const images = ['/images/food/image4.png', '/images/food/image5.png', '/images/food/image1.png', '/images/food/image2.png', '/images/food/image3.png'];
@@ -26,6 +59,7 @@ function Recipies() {
 
   //I used this to make the code a little cleaner.
   let value =  (selectedImageIndex) > 0 ? (selectedImageIndex) % 5 : (selectedImageIndex) < 0 ? (images.length + (selectedImageIndex)) % 5 : (selectedImageIndex);
+  const selectedCard = cardData.find(card => card.key === value);
 
   return (
     <div className='flex flex-col overflow-hidden'>
@@ -41,9 +75,7 @@ function Recipies() {
           <div className=" text-left text-slate-800 text-2xl font-semibold font-['Poppins']">{title[value]}</div>
         </div>
           <div className=' z-10'>
-          {cardData.map((card, index) => (
-            <Card key={index} {...card} />
-          ))}
+          <Card {...selectedCard} />
           </div>
           <div className='scale-[65%] -ml-40 -mt-10'>
             <img src={images[value]} alt="img" />
