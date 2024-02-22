3cea60a678d46a356bcf4705212c041553a70d5d
Stoic Coder <shubhmehrotra2022@vitbhopal.ac.in>
2024-02-23 03:57:48 +0530

    reverted

--- src/components/LandingPage/Landingpage.jsx
+++ src/components/LandingPage/Landingpage.jsx
@@ -23,7 +23,7 @@ function Landingpage() {
         <h1 className="font-Poppins text-9xl font-bold leading-normal mb-[-30px]">Chefio.</h1>
         <h3 className="font-Poppins text-[24px] font-semibold leading-normal m-0 w-[500px]">The community that celebrates cooking</h3>
         <div className="flex justify-start gap-[40px] mt-[20px]">
-          <NavLink to='/chefio-global'><button className="bn5 rounded-lg bg-[rgba(0,0,0,0.17)] px-10 py-4 text-white cursor-pointer font-semibold hover:bg-slate-600 hover:scale-105 transition duration-300 ease-in-out relative">Sign up</button></NavLink>
+          <NavLink to='/'><button className="bn5 rounded-lg bg-[rgba(0,0,0,0.17)] px-10 py-4 text-white cursor-pointer font-semibold hover:bg-slate-600 hover:scale-105 transition duration-300 ease-in-out relative">Sign up</button></NavLink>
           <button className="bn4 rounded-lg px-10 py-4 bg-white text-black cursor-pointer font-semibold hover:bg-zinc-300 hover:scale-105 hover:border-zinc-300 transition duration-300 ease-in-out relative">Sign in</button>
         </div>
       </div>
